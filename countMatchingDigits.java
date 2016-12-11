8a1c90f54c59659495913de462f60142d62f9ba7
hunteryuh <yu59361@gmail.com>
2016-12-10 21:13:13 -0500

    Create countMatchingDigits.java

---
+++ countMatchingDigits.java
@@ -0,0 +1,46 @@
+// Bluej project: lesson6/counting_matches_digits
+/**
+ * A class that represents a number and can report
+ * properties of the number.
+ */
+public class Number
+{
+    private long number;
+
+    public Number(long number)
+    {
+        this.number = number;
+    }
+
+    /**
+     * Returns the number of times digitToMatch appears
+     * in this number.
+     * @param digitToMatch the digit to search for.
+     *   must be at least 0 and at most 9.
+     * @return number of occurrences of digitToMatch
+     */
+    public int countMatchingDigits(int digitToMatch)
+    {
+        // TODO: count and return the number of times the given digit appears in the number
+
+        // Hint: The last digit is (int)(numbers % 10).
+        
+        int count = 0;
+        int digit;
+        long temp = number;
+        if ( number == 0 && digitToMatch == 0){
+            return 1;
+        }else {
+            while( temp>0){
+                digit = (int)(temp%10);
+                
+                
+                if(digit==digitToMatch){
+                    count++;
+                }
+                temp = temp/10;
+            }
+            return count;
+        }
+    }
+}

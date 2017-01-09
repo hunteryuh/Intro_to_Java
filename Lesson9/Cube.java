e7c4298a12e87f94c5504c89937df0c7920bfeaa
hunteryuh <yu59361@gmail.com>
2017-01-09 15:46:03 -0500

    Create Cube.java

---
+++ Lesson9/Cube.java
@@ -0,0 +1,24 @@
+// formulas for surface area and volume
+// http://math2.org/math/geometry/areasvols.htm
+//
+// TODO: complete the code so that Cube implements the GeometricSolid interface
+//
+public class Cube implements GeometricSolid
+{
+    private double side;
+
+    public Cube(double s)
+    {
+        side = s;
+    }
+
+    // other methods
+    public double getVolume(){
+         return side* side*side;
+    }
+    public double getSurfaceArea(){
+        return side*side*6;
+    }
+
+
+}

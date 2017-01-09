1e9ad24d44e2c65a50c14ea167b3e0bb5cfc10ba
hunteryuh <yu59361@gmail.com>
2017-01-09 15:45:46 -0500

    Create Sphere.java

---
+++ Lesson9/Sphere.java
@@ -0,0 +1,45 @@
+// formulas for surface area and volume
+// http://math.about.com/od/formulas/ss/surfaceareavol.htm
+//
+// TODO: complete the code so that Sphere implements the GeometricSolid interface
+//
+public class Sphere implements GeometricSolid
+{
+    private double radius;
+
+    /**
+     * Constructor for objects of class Sphere
+     */
+    public Sphere()
+    {
+        radius = 0;
+    }
+
+    /**
+     * Constructs a Shpere with the given radius
+     * @param r the radius
+     */
+    public Sphere( double r)
+    {
+        radius = r;
+    }
+
+    public void setRadius(double r)
+    {
+        radius = r;
+    }
+
+    public double getRadius()
+    {
+        return radius;
+    }
+
+    //other methods
+    public double getVolume(){
+         return Math.PI*radius*radius*radius*4/3;
+    }
+    public double getSurfaceArea(){
+        return 4* Math.PI * radius * radius;
+    }
+
+}

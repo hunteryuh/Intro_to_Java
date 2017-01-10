d12c49809ff5c545f9998fbb843223e8458241c5
hunteryuh <yu59361@gmail.com>
2017-01-09 22:31:31 -0500

    Create VoltageDividerAmplifier.java

---
+++ Lesson9/VoltageDividerAmplifier.java
@@ -0,0 +1,20 @@
+
+public class VoltageDividerAmplifier extends Amplifier
+{
+     // TODO: supply the implementation of VoltageDividerAmplifier as a subclass of Amplifier
+     // R1 and R2 represent resistance. The gain (g) is calculated from the resistance as follows:
+     // VoltageDividerAmplifier:    g = R2 / (R1 + R2)
+     // TODO: the getGain method needs to return the correct gain (g)
+    private int r1;
+    private int r2;
+        public  VoltageDividerAmplifier( int resistance1, int resistance2)
+    {
+        super(resistance1, resistance2);
+    }
+    public double getGain()
+    {
+        double r1d = (double) (getR1());
+        double r2d = (double) (getR2());
+        return r2d/(r1d+ r2d);
+    }    
+}
